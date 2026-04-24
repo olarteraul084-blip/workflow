637bd2d5d46d723a04bfbb87482a0041f632b10a
Peter Wielander <mittgfu@gmail.com>
2026-04-24 13:28:13 -0700

    WIP remove unrelate dchanges
    Signed-off-by: Peter Wielander <mittgfu@gmail.com>

--- docs/components/worlds/WorldTestingPerformanceMDX.tsx
+++ docs/components/worlds/WorldTestingPerformanceMDX.tsx
@@ -14,11 +14,6 @@ export function WorldTestingPerformanceMDX({
 }) {
   const { worldId, world, meta } = useWorldData();
   return (
-    <WorldTestingPerformance
-      worldId={worldId}
-      world={world}
-      meta={meta}
-      showBenchmarks={showBenchmarks}
-    />
+    <WorldTestingPerformance worldId={worldId} world={world} meta={meta} showBenchmarks={showBenchmarks} />
   );
 }
